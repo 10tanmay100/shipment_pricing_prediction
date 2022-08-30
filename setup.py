698f1ba15f5b37b1bb44b92868e91dc36ca4c03e
unknown <chakrabortytanmay744@gmail.com>
2022-08-30 23:50:17 +0530

    action added basic

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def get_requirements_list()->List[str]:
     This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
     '''
     with open("requirements.txt","r") as f:
-        return f.readlines().remove("-e .")
+        return f.readlines()
 
 setup(
 name="shipping-price-predictor",
-version="0.0.1",
+version="0.0.3",
 author="Tanmay",
 description="This is cover type predictor setup file",
-packages=["shipping_price_predictor"],
+packages=["supply_chain"],
 install_requires=get_requirements_list()
 )
